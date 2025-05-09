b62bf03871b4be82691ba269b86a87d70eca48d5
Tonz <toniabtoniab@gmail.com>
2025-05-08 19:25:44 -0700

    load local json file to avoid http insecurity errors

--- ex-json/script.js
+++ ex-json/script.js
@@ -3,17 +3,12 @@ const spaceContainerISS = document.getElementById("craft-iss");
 const spaceContainerTian = document.getElementById("craft-tiangong");
 const allCrafts = document.querySelectorAll(".craft");
 
-const astrodatas = {"people": [{"craft": "ISS", "name": "Oleg Kononenko"}, {"craft": "ISS", "name": "Nikolai Chub"}, {"craft": "ISS", "name": "Tracy Caldwell Dyson"}, {"craft": "ISS", "name": "Matthew Dominick"}, {"craft": "ISS", "name": "Michael Barratt"}, {"craft": "ISS", "name": "Jeanette Epps"}, {"craft": "ISS", "name": "Alexander Grebenkin"}, {"craft": "ISS", "name": "Butch Wilmore"}, {"craft": "ISS", "name": "Sunita Williams"}, {"craft": "Tiangong", "name": "Li Guangsu"}, {"craft": "Tiangong", "name": "Li Cong"}, {"craft": "Tiangong", "name": "Ye Guangfu"}], "number": 12, "message": "success"};
-
-
 function checkForAstronauts(button) {
     button.style.display = "none"; // hide button
 
-    fetch('http://api.open-notify.org/astros.json') // get astronaut data
+    //fetch('https://api.open-notify.org/astros.json') // vercel blocks http insecure
+    fetch('/ex-json/astros.json')
         .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
             return response.json();
         })
         .then(data => {
